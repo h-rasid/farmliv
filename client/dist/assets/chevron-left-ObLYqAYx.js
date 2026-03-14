bf69b655ea727b97a407d076285b2523301aa9f9
Harunar Rasid <h-rasid>
2026-03-15 00:59:11 +0530

    Synchronized built assets for production update

---
+++ client/dist/assets/chevron-left-ObLYqAYx.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-CgL1UC5E.js";const t=e("ChevronLeft",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{t as C};
--- client/dist/assets/chevron-right-CWvRBpfi.js
+++
@@ -1,1 +0,0 @@
-import{d as t}from"./index-Cu3BhDHe.js";const h=t("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C};
